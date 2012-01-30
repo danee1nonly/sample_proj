6dd1e2990b6cf61f6ad4f8d8c63b18d4096a6cf8
Danny Yuan <dyuan90@gmail.com>
2012-01-30 12:52:32 +0000

    added user microposts

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -9,6 +9,10 @@ module SessionsHelper
     @current_user = user
   end
   
+  def current_user?(user)
+    @current_user == user  
+  end
+  
   def current_user
     @current_user ||= user_from_remember_token
   end
